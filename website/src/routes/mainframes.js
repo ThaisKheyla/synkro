4fffa55706dcf3b968d0360adf7a05bbc00f0a81
Bianca-LOrtega <bianca.ortega@sptech.school>
2025-10-24 20:04:22 -0300

    back mainframe

--- website/src/routes/mainframes.js
+++ website/src/routes/mainframes.js
@@ -6,8 +6,16 @@ var mainframesController = require("../controllers/mainframesController");
 router.get("/setores", mainframesController.listarSetores);
 router.get("/sistemas", mainframesController.listarSistemas);
 router.get("/componentes", mainframesController.listarComponentes);
+router.get("/tipos", mainframesController.listarTipos);
+
+router.post("/setores", mainframesController.cadastrarSetor);
+router.post("/sistemas", mainframesController.cadastrarSistema);
+router.post("/componentes", mainframesController.cadastrarComponente);
+router.post("/tipos", mainframesController.cadastrarTipo);
 
 router.post("/", mainframesController.cadastrarMainframe);
+
+router.get("/empresa/:idEmpresa", mainframesController.listarPorEmpresa);
 
 router.get("/", mainframesController.listarMainframes);
 
