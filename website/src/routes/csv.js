2214d5e8a010d5dc31241c3d35ffc61a5e326f17
hilukasp <lucaskawakami5@gmail.com>
2025-12-02 17:34:52 -0300

    dashboard processo

--- website/src/routes/csv.js
+++ website/src/routes/csv.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/csvController");
 
 router.get('/processo/:macAdress',controller.buscarCSV)
+router.get('/dadoscsv/:arquivo/:macAdress',controller.buscarCSVAWS)
+
 
 module.exports = router;
